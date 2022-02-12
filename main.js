71aa066f4a108e3274c2a6cbd8a728eac8b1dfda
Sudipta <hisudipta26@gmail.com>
2022-02-12 23:41:28 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,42 @@
+const initialPrice = document.querySelector('#initial-price');
+const quantityOfStocks = document.querySelector('#quantity');
+const currentPrice = document.querySelector('#current-price');
+const tellMeButton = document.querySelector('#tell-me-button');
+const errorMessage = document.querySelector('.error');
+const outputMessage = document.querySelector('.output');
+
+tellMeButton.addEventListener('click', function tellMeHandler() {
+    var ip = Number(initialPrice.value);
+    var qty = Number(quantityOfStocks.value);
+    var cp = Number(currentPrice.value);
+    calculateProfitAndLoss(ip, qty, cp);
+});
+
+function calculateProfitAndLoss(initial, quantity, current) {
+    if (initialPrice.value == "" || quantityOfStocks.value == "" || currentPrice.value == "") {
+        outputMessage.style.display = "none";
+        errorMessage.style.display = "block";
+        errorMessage.innerText = "Please fill out all the required fields!";
+    } else {
+        if (initial < current) {
+            var profit = (current - initial) * quantity;
+            var profitPercentage = (profit / (initial * quantity)) * 100;
+            errorMessage.style.display = "none";
+            outputMessage.style.display = "block";
+            outputMessage.style.color = 'green';
+            outputMessage.innerText = `Hey, the Profit is ${profit} and the profit percentage is ${profitPercentage}%`;
+        } else if (initial > current) {
+            var loss = (initial - current) * quantity;
+            var lossPercentage = (loss / (initial * quantity)) * 100;
+            errorMessage.style.display = "none";
+            outputMessage.style.display = "block";
+            outputMessage.style.color = 'orange';
+            outputMessage.innerText = `Oops!, the Loss is ${loss} and the loss percentage is ${lossPercentage}%`;
+        } else {
+            errorMessage.style.display = "none";
+            outputMessage.style.display = "block";
+            outputMessage.style.color = 'blue';
+            outputMessage.innerText = "No pain no gain and no gain no pain :)";
+        }
+    }
+}
